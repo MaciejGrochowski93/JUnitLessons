85970048c6082d750b628c8dcc8daf479d421252
Maciej <maciejgrochowski93@o2.pl>
2021-07-22 23:55:42 +0200

    adding more tests

--- src/main/java/maciej/grochowski/MathUtils.java
+++ src/main/java/maciej/grochowski/MathUtils.java
@@ -5,4 +5,20 @@ public class MathUtils {
     public int add(int x, int y) {
         return x + y;
     }
+
+    public int subtract(int x, int y) {
+        return x - y;
+    }
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public int divide(int x, int y) {
+        return x / y;
+    }
+
+    public double calculateCircleArea(double radius) {
+        return Math.PI * radius * radius;
+    }
 }
--- src/test/java/maciej/grochowski/MathUtilsTest.java
+++ src/test/java/maciej/grochowski/MathUtilsTest.java
@@ -2,7 +2,8 @@ package maciej.grochowski;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MathUtilsTest {
 
@@ -19,4 +20,20 @@ class MathUtilsTest {
         actual = utils.add(2, 1);
 
         assertEquals(expected, actual);
-    }}
+    }
+
+    @Test
+    void testCalculateCircleArea() {
+        MathUtils utils = new MathUtils();
+
+        assertEquals(314.1592653589793, utils.calculateCircleArea(10), "Should return correct circle area");
+    }
+
+    @Test
+    void divideTest() {
+        MathUtils utils = new MathUtils();
+
+        assertThrows(ArithmeticException.class, () -> utils.divide(2, 0), "Divide by 0 should throw Arithmetic exception");
+        
+    }
+}
