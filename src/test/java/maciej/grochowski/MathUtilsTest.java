b031606a609cefa7b5b38094f1ad5253d651766c
Maciej <maciejgrochowski93@o2.pl>
2021-07-23 22:09:48 +0200

    adding some new test examples

--- src/test/java/maciej/grochowski/MathUtilsTest.java
+++ src/test/java/maciej/grochowski/MathUtilsTest.java
@@ -1,16 +1,43 @@
 package maciej.grochowski;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest {
 
-    @Test
-    void name() {
-        MathUtils utils = new MathUtils();
+    MathUtils utils;
+    int a = 1;
 
+//    @BeforeAll
+//    static void beforeAllInit() {
+//        System.out.println("BeforeAll starting");
+//    }
+//
+//    @AfterAll
+//    static void afterAllCleanUp() {
+//        System.out.println("AfterAll starting");
+//    }
+
+    @BeforeEach
+    void init() {
+        utils = new MathUtils();
+    }
+
+//    @AfterEach
+//    void cleanUp() {
+//        a++;
+//        System.out.println("Cleaning up");
+//        System.out.println("a = " + a + "\n");
+//    }
+
+    @Test
+    @DisplayName("Testing add() method")
+    void addTest() {
         int expected = 2;
         int actual = utils.add(1, 1);
 
@@ -23,17 +50,23 @@ class MathUtilsTest {
     }
 
     @Test
-    void testCalculateCircleArea() {
-        MathUtils utils = new MathUtils();
+    @Disabled
+    @DisplayName("TDD test, shouldn't run")
+    void testDisabled() {
+        fail("Should be disabled");
+//        System.out.println("It shouldn't appear");
+    }
 
+    @Test
+    void calculateCircleAreaTest() {
+        boolean isRegistered = false;
+        assumeTrue(isRegistered);
         assertEquals(314.1592653589793, utils.calculateCircleArea(10), "Should return correct circle area");
     }
 
     @Test
+    @EnabledOnOs(OS.MAC)
     void divideTest() {
-        MathUtils utils = new MathUtils();
-
         assertThrows(ArithmeticException.class, () -> utils.divide(2, 0), "Divide by 0 should throw Arithmetic exception");
-        
     }
 }
